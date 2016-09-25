4db7e5967139ab97c1ca0aee31d7c0d782b1255c
Zaka Elab <zakaelab@openmailbox.org>
2016-09-25 03:10:04 +0200

    Transform dates to ISO 8601 format

--- process-files.py
+++ process-files.py
@@ -1,23 +1,40 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import datetime as dt
-import pdb
+import pandas as pd
+import numpy as np
 
-print "Processing..."
+print("Processing...")
 
-with open('wikipedia-trends-bitcoin-may-2016.csv','r') as f:
-    lines = f.readlines()
+# Transform dates to ISO 8601 date format
 
-with open('wikipedia-trends-bitcoin-may-2016-formatted.csv','w') as f:
-    f.write('Date,Value\n')
-    
-    for line in lines[1:]:
-        dateStr, value = line.split(',')
-        formattedDate = dt.datetime.strptime(dateStr,
-                        "%Y/%m/%d").strftime("%d/%m/%Y %H:%M:%S")
+files = [ 'bitcoin-days-destroyed.csv', 'blocks-size.csv',
+          'cost-per-transaction-percent.csv',
+          'cost-per-transaction.csv', 'difficulty.csv',
+          'estimated-transaction-volume-usd.csv',
+          'estimated-transaction-volume.csv',
+          'euro-price-in-usd.csv', 'hash-rate.csv',
+          'market-cap.csv', 'market-price.csv',
+          'median-confirmation-time.csv',
+          'miners-revenue.csv',
+          'n-transactions-excluding-chains-longer-than-10.csv',
+          'n-transactions-excluding-chains-longer-than-100.csv',
+          'n-transactions-excluding-chains-longer-than-1000.csv',
+          'n-transactions-excluding-chains-longer-than-10000.csv',
+          'n-transactions-excluding-popular.csv',
+          'n-transactions-multiple.csv',
+          'n-transactions-per-block.csv',
+          'n-transactions-total.csv', 'n-transactions.csv',
+          'n-unique-addresses.csv', 'network-deficit.csv',
+          'output-volume.csv', 'standard-and-poors-500.csv',
+          'total-bitcoins.csv', 'trade-volume.csv',
+          'transaction-fees-usd.csv', 'transaction-fees.csv',
+          'tx-trade-ratio.csv',
+          'wikipedia-trends-bitcoin-may-2016.csv' ]
 
-        formattedLine = formattedDate + "," + value
+for f in files:
+    df = pd.read_csv(f, parse_dates=True,
+                     dayfirst=True, index_col=0)
 
-        f.write(formattedLine)
+    df.to_csv(f)
 
-print "Finished."
+print("Finished!!")
